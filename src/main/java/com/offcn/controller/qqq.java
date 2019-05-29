e81997c0e111e1b98860f060e8699e52301478a8
wweixing <1685683784@qq.com>
2019-05-29 16:09:11 +0800

    修改了下qqq

--- src/main/java/com/offcn/controller/qqq.java
+++ src/main/java/com/offcn/controller/qqq.java
@@ -1,5 +1,13 @@
 package com.offcn.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class qqq {
+	@RequestMapping("/qqq")
+	public String qqq(){
+		return "hello word!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+	}
 
 }
